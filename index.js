ecf984478876546070b7dde6c540add4c7315d46
Rafatbot <108188737+Rafatbot@users.noreply.github.com>
2022-07-09 03:04:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -145,8 +145,8 @@ Kon = await getBuffer(`https://hardianto.xyz/api/welcome3?profile=${encodeURICom
 Tol = await getBuffer(`https://hardianto.xyz/api/goodbye3?profile=${encodeURIComponent(ppuser)}&name=${encodeURIComponent(nama)}&bg=https://telegra.ph/file/8bbe8a7de5c351dfcb077.jpg&namegb=${encodeURIComponent(metadata.subject)}&member=${encodeURIComponent(memb)}`)
                 if (anu.action == 'add') {
                     GojoMdNx.sendMessage(anu.id, { image: Kon, contextInfo: { mentionedJid: [num] }, caption: `
-⭐✑ Hi👋 @${num.split("@")[0]},
-⭐✑ Welcome To ${metadata.subject}
+⭐✑ مرحباً👋 @${num.split("@")[0]},
+⭐✑ مرحباَ بك في ${metadata.subject}
 
 ⭐✑ Description: ${metadata.desc}
 
